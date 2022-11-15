0e1300df1b5160a798737ed5cc5bf4f4ccc033f8
adriannaliu <72051204+adriannaliu@users.noreply.github.com>
2022-11-15 12:00:02 -0500

    tried something with image

--- controllers/tuits/tuits-controller.js
+++ controllers/tuits/tuits-controller.js
@@ -5,7 +5,9 @@ const createTuit = (req, res) => {
   const newTuit = req.body;
   newTuit._id = (new Date()).getTime() + '';
   newTuit.likes = 0;
+  newTuit.dislikes = 0;
   newTuit.liked = false;
+  newTuit.image = "/Users/adriannaliu/Documents/NEU/Year4/webdev/tuiter-react-web-app/public/images/nasa.png";
   tuits.push(newTuit);
   res.json(newTuit);
 }
